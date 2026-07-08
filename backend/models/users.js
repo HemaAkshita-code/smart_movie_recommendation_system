08dcec483907e89d824f31818f1dc16154d06012
HemaAkshita-code <hemaakshitav@gmail.com>
2026-07-08 17:50:29 +0530

    email verification added

--- backend/models/users.js
+++ backend/models/users.js
@@ -11,7 +11,10 @@ const userLoginSchema = mongoose.Schema({
     email : {type: String, unique: true, required: true, lowercase: true, trim: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/},
     password : {type: String, required: true},
     created_at : {type: Date, default: Date.now},
-    updated_at : {type: Date, default: Date.now}
+    updated_at : {type: Date, default: Date.now},
+    isVerified : {type: Boolean, default: false},
+    otp : {type: String, default: null},
+    otp_expiration : {type: Date, default: null}
 })
 
 const UserLoginModel = mongoose.model('UserLogin', userLoginSchema);
