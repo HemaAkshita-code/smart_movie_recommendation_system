10ddf4f9e696d7b32fd05af1cb0c44d8b3e08fd7
HemaAkshita-code <hemaakshitav@gmail.com>
2026-08-09 12:49:28 +0530

    recommendation controller

--- backend/controllers/recommendationControllers.js
+++ backend/controllers/recommendationControllers.js
@@ -2,5 +2,11 @@
 
 async function recommend(req, res, next) 
 {
+    const FinalScore =
+        // 0.60 * embeddingSimilarity
+        // + 0.25 * featureSimilarity
+        // + 0.10 * popularityBoost
+        // + 0.05 * recencyBoost
 
+        
 }
